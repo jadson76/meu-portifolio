2f6072768477a334750455c28f550191c7ec5657
Jadson Souza Faria <biojadson@gmail.com>
2024-01-30 08:49:23 -0300

    Add Dialog component

---
+++ src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { IProjects } from '../../../interface/IProjects.interface';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-projects',
+  standalone: true,
+  imports: [MatDialogModule],
+  templateUrl: './dialog-projects.component.html',
+  styleUrl: './dialog-projects.component.scss'
+})
+export class DialogProjectsComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<DialogProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: IProjects
+   ){}
+
+
+  ngOnInit(): void {
+   this.getData.set(this._data);
+  }
+
+  public getData = signal<IProjects | null>(null);
+
+  public closeModal(){
+    return this.dialogRef.close();
+  }
+
+}
--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,14 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  #dialog = inject(MatDialog)
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: "assets/img/projects/vfull.png",
@@ -57,4 +64,13 @@ export class ProjectsComponent {
     },
 
   ])
+
+  public openDialog(data: IProjects) {
+
+    this.#dialog.open(DialogProjectsComponent,{
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
+
 }
